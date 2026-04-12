e75a691093e2694a59ad2cef29560386e9231fb4
Neha-Nimsatkar <1492neha@gmail.com>
2026-04-12 10:33:12 +0530

    updated airflow dag

--- batch_pipeline/orchestration/databricks_workflow.py
+++ batch_pipeline/orchestration/databricks_workflow.py
@@ -7,5 +7,5 @@ with DAG('databricks_dag', start_date=datetime(2023, 1, 1), schedule_interval=No
     run_job = DatabricksRunNowOperator(
         task_id='run_my_databricks_job',
         databricks_conn_id='databricks_default',
-        job_id=123456789  # Replace with your actual Databricks Job ID
+        job_id=294840511200667  # Replace with your actual Databricks Job ID
     )
