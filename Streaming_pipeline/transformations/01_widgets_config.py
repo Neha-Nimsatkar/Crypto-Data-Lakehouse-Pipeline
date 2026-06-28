f28b85d02f504ce19da320844cf49ff6feacc75c
nehanimsatkar14 <nimsatkarneha2004@gmail.com>
2026-06-28 22:37:41 +0530

    correcting syntax of file gold validations

--- Streaming_pipeline/transformations/01_widgets_config.py
+++
@@ -1,6 +0,0 @@
-# Cell 1: Creating UI Input Boxes (Widgets) inside your Serverless Notebook
-dbutils.widgets.text("kafka_bootstrap_server", "", "1. Kafka Bootstrap Server")
-dbutils.widgets.text("kafka_api_key", "", "2. Kafka API Key")
-dbutils.widgets.text("kafka_api_secret", "", "3. Kafka API Secret")
-
-print(" Look at the very top of your notebook! You will see 3 text boxes.")
